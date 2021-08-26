a28edab74bf5435bb01bfbcdd841d3bb2b4006c8
Việt Anh Bùi <vietanhbui2000@outlook.com>
2021-08-26 16:53:20 +0700

    shared: update
    Adapt to MiWiFi-R1CM.

--- release/src/router/shared/sysdeps/ralink/mt7620.c
+++ release/src/router/shared/sysdeps/ralink/mt7620.c
@@ -91,12 +91,12 @@ enum {
 };
 #elif defined(RTAC1200HP)
 enum {
-	WAN_PORT=5,
-	LAN1_PORT=0,
-	LAN2_PORT=1,
-	LAN3_PORT=2,
-	LAN4_PORT=3,
-	P5_PORT=4,
+	WAN_PORT=4,
+	LAN1_PORT=1,
+	LAN2_PORT=0,
+	LAN3_PORT=3,
+	LAN4_PORT=2,
+	P5_PORT=5,
 	CPU_PORT=6,
 	P7_PORT=7,
 };
